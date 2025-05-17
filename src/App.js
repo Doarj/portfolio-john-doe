05cf6810a56f1f9ec15447d0c480e67d17cbbd5d
Doarj <jeremok06@gmail.com>
2025-05-17 11:58:06 +0200

    Ajouter le routage et les pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import { Route, Routes, Link } from 'react-router-dom';
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import Realisations from "./pages/Realisations";
+import Accueil from "./pages/Accueil";
+import MentionsLegales from "./pages/MentionsLegales";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Routes>
+        <Route path="/" element = {<Accueil/>}></Route>
+        <Route path="services" element = {<Services/>}></Route>
+        <Route path="contact" element = {<Contact/>}></Route>
+        <Route path="realisations" element = {<Realisations/>}></Route>
+        <Route path="mentionslegales" element = {<MentionsLegales/>}></Route>
+      </Routes>
     </div>
   );
 }
