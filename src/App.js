c7412d0c4506893bbd188ff6c95410019700d1be
Doarj <jeremok06@gmail.com>
2025-06-06 11:20:22 +0200

    add footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { Route, Routes, Link } from 'react-router-dom';
-import { Nav, Navbar, Container } from 'react-bootstrap';
+import { Nav, Navbar, Container, Row, Col } from 'react-bootstrap';
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
 import Realisations from "./pages/Realisations";
@@ -17,11 +17,6 @@ export default function App () {
                   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="ms-auto text-uppercase" variant="underline">
-                            {/*<Link to="/" className="nav-link">Accueil</Link>
-                            <Link to="/services" className="nav-link">Services</Link>
-                            <Link to="/realisations" className="nav-link">Portfolio</Link>
-                            <Link to="/contact" className="nav-link">Contact</Link>
-                            <Link to="/mentionslegales" className="nav-link">Mentions Légales</Link>*/}
                             <Nav.Link as={Link} to="/">Accueil</Nav.Link>
                             <Nav.Link as={Link} to="/services">Services</Nav.Link>
                             <Nav.Link as={Link} to="/realisations">Portfolio</Nav.Link>
@@ -44,6 +39,44 @@ export default function App () {
         <Route path="realisations" element = {<Realisations/>}></Route>
         <Route path="mentionslegales" element = {<MentionsLegales/>}></Route>
       </Routes>
+
+
+      <footer>
+        <Container fluid>
+            <Row className = "bg-dark text-light p-4">
+                <Col className="mx-5 mb-4 colFooter">
+                    <h3>John Doe</h3>
+                    <ul className="list-group list-unstyled">
+                        <li>38, Rue de Lille</li>
+                        <li>75007 Paris, France</li>
+                        <li>06 06 06 06 06</li>
+                        <li>johndoe@gmail.com</li>
+                    </ul>
+                    <a href="https://github.com/github-john-doe" target="blank" className="link-light"><i class="bi bi-github fs-2 icone" rel="nofollow"></i></a>
+                    <a href="https://x.com/fr" target="blank" className="link-light"><i class="bi bi-twitter-x fs-2 mx-3 icone" rel="nofollow"></i></a>
+                    <a href="https://www.linkedin.com/in/johndoes/" target="blank" className="link-light"><i class="bi bi-linkedin fs-2 icone" rel="nofollow"></i></a>
+                </Col>
+
+                <Col className="mx-5 mb-4 colFooter">
+                <h3>Liens utiles</h3>
+                    <ul className="list-group list-unstyled">
+                        <Link to="/" className="nav-link linkFooter">Home</Link>
+                        <Link to="/services" className="nav-link linkFooter">Services</Link>
+                        <Link to="/realisations" className="nav-link linkFooter" rel="noindex">Portfolio</Link>
+                        <Link to="/contact" className="nav-link linkFooter">Contact</Link>
+                        <Link to="/mentionslegales" className="nav-link linkFooter">Mentions Légales</Link>
+                    </ul>                        
+                </Col>
+
+                <Col className="mx-5 mb-4 colFooter">
+                <h3>Mes dernières réalisations</h3>
+                    <ul className="list-group list-unstyled">
+                        
+                    </ul>         
+                </Col>
+            </Row>
+        </Container>
+            </footer>
     </div>
   );
 }
