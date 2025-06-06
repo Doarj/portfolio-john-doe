4e66713f6cba63d94b5fbdafc04e3be50e3bf7b7
Doarj <jeremok06@gmail.com>
2025-06-06 10:59:31 +0200

    navbar + link

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import './App.css';
 import { Route, Routes, Link } from 'react-router-dom';
+import { Nav, Navbar, Container } from 'react-bootstrap';
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
 import Realisations from "./pages/Realisations";
 import Accueil from "./pages/Accueil";
 import MentionsLegales from "./pages/MentionsLegales";
 
-function App() {
+export default function App () {
   return (
     <div>
+      <header>
+        <Navbar collapseOnSelect expand="lg" bg="dark" data-bs-theme="dark" fixed="top">
+          <Container>
+              <Navbar.Brand as={Link} to="/">John Doe</Navbar.Brand>
+                  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ms-auto text-uppercase" variant="underline">
+                            {/*<Link to="/" className="nav-link">Accueil</Link>
+                            <Link to="/services" className="nav-link">Services</Link>
+                            <Link to="/realisations" className="nav-link">Portfolio</Link>
+                            <Link to="/contact" className="nav-link">Contact</Link>
+                            <Link to="/mentionslegales" className="nav-link">Mentions Légales</Link>*/}
+                            <Nav.Link as={Link} to="/">Accueil</Nav.Link>
+                            <Nav.Link as={Link} to="/services">Services</Nav.Link>
+                            <Nav.Link as={Link} to="/realisations">Portfolio</Nav.Link>
+                            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+                            <Nav.Link as={Link} to="/mentionslegales">Mentions Légales</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+            </Container>
+          </Navbar>
+      </header>
+      <main>
+
+      </main>
+
+
       <Routes>
         <Route path="/" element = {<Accueil/>}></Route>
         <Route path="services" element = {<Services/>}></Route>
@@ -20,4 +48,4 @@ function App() {
   );
 }
 
-export default App;
+
---
+++ src/pages/Accueil.jsx
@@ -0,0 +1,15 @@
+import "../App.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Accueil = ()=>{
+    return (
+        <div className="App-body">
+        <h1>Ma page APP</h1>
+        <h2>Accueil</h2>
+        
+        
+        </div> 
+
+    )
+}
+export default Accueil;
