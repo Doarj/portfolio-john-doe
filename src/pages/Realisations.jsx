d3436df6bd7813304caa894a6b36b483e0ac7a10
Doarj <jeremok06@gmail.com>
2025-06-09 13:41:16 +0200

    Add portfolio

--- src/pages/Realisations.jsx
+++ src/pages/Realisations.jsx
@@ -1,12 +1,131 @@
 import "../App.css";
+import { Container, Row, Col, Card, Image, Button } from "react-bootstrap";
+import Fresh from "../assets/img/portfolio/fresh-food.jpg";
+import Akira from "../assets/img/portfolio/restaurant-japonais.jpg";
+import Seo from "../assets/img/portfolio/seo.jpg";
+import CreationApi from "../assets/img/portfolio/coder.jpg";
+import Maquette from "../assets/img/portfolio/screens.jpg";
+import Etre from "../assets/img/portfolio/espace-bien-etre.jpg";
+import Banner from "../assets/img/banner.jpg";
 
-
-const Realisations = ()=>{
+export default function Realisations () {
   return (
-      <div>
-      <h1>Realisations</h1>
-      </div> 
+    <div>
+      <Image src={Banner} fluid className="mt-5"/>
+      <Container className="pt-5 text-center">
+            <h2 className="fw-bold">Portfolio</h2>
+            <p>Voici quelques-unes de mes réalisations</p>
+            <Row>
+                <Col></Col>
+                <Col><hr className="border border-primary border-3 opacity-100 mb-5"/></Col>
+                <Col></Col>     
+            </Row>
+      </Container>
+      <Container className="text-center">
+          <Row className="mb-3 ">
 
-  )
+            <Col className="col-md-4">
+              <Card className="mb-4 AppCard">
+                <Card.Img variant="top" src= {Fresh} title="Fresh Food" alt="fruits et légumes dans une cagette"/>
+                <Card.Body>
+                  <Card.Title className="fw-bold">Fresh Food</Card.Title>
+                  <Card.Text>
+                    <small className="CardText">Site de vente de produits frais en ligne</small>
+                  </Card.Text>
+                  <Button className="btn-portfolio">Voir le site</Button>
+                </Card.Body>
+                <Card.Footer>
+                  <small className="text-muted">Site réalisé avec PHP et MySQL</small>
+                </Card.Footer>
+              </Card>
+            </Col>
+
+            <Col className="col-md-4">
+              <Card className="mb-4 AppCard">
+                <Card.Img variant="top" src= {Akira} title="Restaurant Akira" alt="photo sushi"/>
+                <Card.Body>
+                  <Card.Title className="fw-bold">Restaurant Akira</Card.Title>
+                  <Card.Text>
+                    <small className="CardText">Site de vente de produits frais en ligne</small>
+                  </Card.Text>
+                  <Button className="btn-portfolio">Voir le site</Button>
+                </Card.Body>
+                <Card.Footer>
+                  <small className="text-muted">Site réalisé avec WordPress</small>
+                </Card.Footer>
+              </Card>
+            </Col>
+
+            <Col className="col-md-4">
+              <Card className="mb-4 AppCard"> 
+                <Card.Img variant="top" src= {Etre} title="Espace Bien-être" alt="photo avec tête de Buddah"/>
+                <Card.Body>
+                  <Card.Title className="fw-bold">Espace bien-être</Card.Title>
+                  <Card.Text>
+                    <small className="CardText">Site de vente de produits frais en ligne</small>
+                  </Card.Text>
+                  <Button className="btn-portfolio">Voir le site</Button>
+                </Card.Body>
+                <Card.Footer>
+                  <small className="text-muted">Site réalisé avec LAVAREL</small>
+                </Card.Footer>
+              </Card>
+            </Col>  
+          </Row>
+
+          <Row className="mb-3">
+
+            <Col className="col-md-4">
+              <Card className="mb-4 AppCard">
+                <Card.Img variant="top" src= {Seo} title="SEO" alt="textes SEO à la verticale et à l'horizontale"/>
+                <Card.Body>
+                  <Card.Title className="fw-bold">SEO</Card.Title>
+                  <Card.Text>
+                    <small className="CardText">Amélioration du référencement d'un site e-commerce</small>
+                  </Card.Text>
+                  <Button className="btn-portfolio">Voir le site</Button>
+                </Card.Body>
+                <Card.Footer>
+                  <small className="text-muted">Utilisation des outils SEO</small>
+                </Card.Footer>
+              </Card>
+            </Col>
+
+            <Col className="col-md-4">
+              <Card className="mb-4 AppCard">
+                <Card.Img variant="top" src= {CreationApi} title="Création API" alt="affichage de code"/>
+                <Card.Body>
+                  <Card.Title className="fw-bold">Création d'une API</Card.Title>
+                  <Card.Text>
+                    <small className="CardText">Création d'une API RESTFULL publique</small>
+                  </Card.Text>
+                  <Button className="btn-portfolio">Voir le site</Button>
+                </Card.Body>
+                <Card.Footer>
+                  <small className="text-muted">PHP - SYMFONY</small>
+                </Card.Footer>
+              </Card>
+            </Col>
+            
+            <Col className="col-md-4">
+              <Card className="mb-4 AppCard">
+                <Card.Img variant="top" src= {Maquette} title="Maquette d'un site" alt="ordinateur PC portable et tablette"/>
+                <Card.Body>
+                  <Card.Title className="fw-bold">Maquette d'un site web</Card.Title>
+                  <Card.Text>
+                    <small className="CardText">Création du prototype d'un site</small>
+                  </Card.Text>
+                  <Button className="btn-portfolio">Voir le site</Button>
+                </Card.Body>
+                <Card.Footer>
+                  <small className="text-muted">Réalisé avec FIGMA</small>
+                </Card.Footer>
+              </Card>
+            </Col>  
+          </Row>
+        </Container>
+
+  </div>
+
+  );
 }
-export default Realisations;
